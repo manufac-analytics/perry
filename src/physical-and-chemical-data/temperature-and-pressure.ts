9cc73d31825fcaba5a215175e4d97d565db21b08
maneetgoyal <maneetgoyal14@gmail.com>
2021-07-15 17:54:04 +0530

    Add temp and pres interfaces

--- src/physical-and-chemical-data/temperature-and-pressure.ts
+++ src/physical-and-chemical-data/temperature-and-pressure.ts
@@ -1,20 +1,19 @@
 export interface Measure {
-    value: number;
-    units: string;
+  value: number;
+  units: string;
 }
 
 export interface MeasureRange {
-    min: Measure;
-    max: Measure;
+  min: Measure;
+  max: Measure;
 }
 
 export interface TemperaturPressureCombo {
-    temperature: Measure;
-    pressure: Measure;
+  temperature: Measure;
+  pressure: Measure;
 }
 
 export interface MolarWaterLoss {
-    waterMoles: number;
-    temperature: Measure;
+  waterMoles: number;
+  temperature: Measure;
 }
-

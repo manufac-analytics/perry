fac48d37be16671b0e769ad4884d5f39571bd5b0
maneetgoyal <maneetgoyal14@gmail.com>
2021-07-15 20:47:00 +0530

    Update MeltingPoint

--- src/physical-and-chemical-data/melting-point.ts
+++ src/physical-and-chemical-data/melting-point.ts
@@ -1,17 +1,24 @@
 import { Measure, MeasureRange } from './utils';
 
-export const MeltingPointNature = {
+export interface TemperaturePressureCombo {
+  temperature: Measure | MeasureRange;
+  pressure: Measure | MeasureRange;
+}
+
+export const MeltingOutcomes = {
   Decomposes: 'decomposes',
   Sublimes: 'sublimes',
   Explodes: 'explodes',
   Melts: 'melts',
   LoosesWater: 'looses-water',
+  Transitions: 'transitions',
+  Deliquesces: 'deliquesces',
 } as const;
-export type MeltingPointNature = typeof MeltingPointNature[keyof typeof MeltingPointNature];
+export type MeltingOutcomes = typeof MeltingOutcomes[keyof typeof MeltingOutcomes];
 
-export interface MeltingPoint {
-  temperature: Measure | MeasureRange;
-  pressure: Measure | MeasureRange;
-  nature: MeltingPointNature;
-  waterMoles?: number;
+export interface MeltingPoint extends TemperaturePressureCombo {
+  nature: MeltingOutcomes;
+  loosesWaterMoles?: number;
+  transitionsTo?: string;
+  sublimesIn?: string;
 }

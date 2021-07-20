93f993b323ef597b41e490c904296e2d0bafa778
maneetgoyal <maneetgoyal14@gmail.com>
2021-07-20 17:54:56 +0530

    Add vapor pressure tests

---
+++ src/physical-and-chemical-data/data/vapor-pressure-constants.ts
@@ -0,0 +1,43 @@
+import { DIPPRVaporPressureProps } from '../interfaces/vapor-pressure';
+
+export const DIPPRVaporPressureDictionary: Record<
+  string,
+  DIPPRVaporPressureProps
+> = {
+  Acetaldehyde: {
+    name: 'Acetaldehyde',
+    formula: 'C2H4O',
+    CAS: '75-07-0',
+    C1: 52.9107,
+    C2: -4643.14,
+    C3: -4.50683,
+    C4: 2.7e-17,
+    C5: 6,
+    minimumTemperature: 149.78,
+    maximumTemperature: 466,
+    pressureAtMinimumTemperature: 5.15e-1,
+    pressureAtMaximumTemperature: 5.57e6,
+  },
+} as const;
+
+export function calculateDIPPRVaporPressure(
+  compound: string,
+  temperature: number
+): number {
+  const vaporPressureProps: DIPPRVaporPressureProps | undefined =
+    DIPPRVaporPressureDictionary[compound];
+  let vaporPressure = NaN;
+  if (
+    vaporPressureProps !== undefined &&
+    temperature >= vaporPressureProps.minimumTemperature &&
+    temperature <= vaporPressureProps.maximumTemperature
+  ) {
+    vaporPressure = Math.exp(
+      vaporPressureProps.C1 +
+      vaporPressureProps.C2 / temperature +
+      vaporPressureProps.C3 * Math.log(temperature) +
+      vaporPressureProps.C4 * Math.pow(temperature, vaporPressureProps.C5)
+    );
+  }
+  return vaporPressure;
+}

4e5fa8c5450148348e25552ca2a101fa91e45f09
maneetgoyal <maneetgoyal14@gmail.com>
2021-07-20 17:55:15 +0530

    Prettify

--- test/vapor-pressure.test.ts
+++ test/vapor-pressure.test.ts
@@ -1,14 +1,37 @@
-import { calculateDIPPRVaporPressure, DIPPRVaporPressureDictionary } from '../src/physical-and-chemical-data/data/vapor-pressure-constants';
+import {
+  calculateDIPPRVaporPressure,
+  DIPPRVaporPressureDictionary,
+} from '../src/physical-and-chemical-data/data/vapor-pressure-constants';
 
 describe('calculateDIPPRVaporPressure', () => {
   it('should return NaN outside the specified range of temperature', () => {
-    const vaporProps = DIPPRVaporPressureDictionary["Acetaldehyde"];
-    expect(calculateDIPPRVaporPressure("Acetaldehyde", vaporProps.minimumTemperature - 1)).toBe(NaN);
-    expect(calculateDIPPRVaporPressure("Acetaldehyde", vaporProps.maximumTemperature + 1)).toBe(NaN);
+    const vaporProps = DIPPRVaporPressureDictionary['Acetaldehyde'];
+    expect(
+      calculateDIPPRVaporPressure(
+        'Acetaldehyde',
+        vaporProps.minimumTemperature - 1
+      )
+    ).toBe(NaN);
+    expect(
+      calculateDIPPRVaporPressure(
+        'Acetaldehyde',
+        vaporProps.maximumTemperature + 1
+      )
+    ).toBe(NaN);
   });
   it('should return correct vapor pressure at Tmin and Tmax', () => {
-    const vaporProps = DIPPRVaporPressureDictionary["Acetaldehyde"];
-    expect(calculateDIPPRVaporPressure("Acetaldehyde", vaporProps.minimumTemperature).toPrecision(3)).toBe(vaporProps.pressureAtMinimumTemperature.toPrecision(3));
-    expect(calculateDIPPRVaporPressure("Acetaldehyde", vaporProps.maximumTemperature).toPrecision(3)).toBe(vaporProps.pressureAtMaximumTemperature.toPrecision(3));
+    const vaporProps = DIPPRVaporPressureDictionary['Acetaldehyde'];
+    expect(
+      calculateDIPPRVaporPressure(
+        'Acetaldehyde',
+        vaporProps.minimumTemperature
+      ).toPrecision(3)
+    ).toBe(vaporProps.pressureAtMinimumTemperature.toPrecision(3));
+    expect(
+      calculateDIPPRVaporPressure(
+        'Acetaldehyde',
+        vaporProps.maximumTemperature
+      ).toPrecision(3)
+    ).toBe(vaporProps.pressureAtMaximumTemperature.toPrecision(3));
   });
 });

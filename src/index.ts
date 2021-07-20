1c5873d785412b1706adb2f5938258d35ee61238
maneetgoyal <maneetgoyal14@gmail.com>
2021-07-20 20:07:48 +0530

    Add data for o-Terphenyl and wtaer densities

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './data/vapor-pressure-constants';
 export * from './data/inorganic-compounds';
 export * from './data/inchi-dictionary';
 export * from './data/food-compounds';
+export * from './data/density-constants';

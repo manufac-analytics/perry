9da9ed5f001df2ab62458010ad53e69eba2ec8db
SumitNegi7 <sumitnegi2142@gmail.com>
2021-07-24 12:54:55 +0530

    Add Vapor pressure

--- src/data/vapor-pressure-constants.ts
+++ src/data/vapor-pressure-constants.ts
@@ -18,6 +18,36 @@ export const DIPPRVaporPressureDictionary: Record<
     pressureAtMinimumTemperature: 5.15e-1,
     pressureAtMaximumTemperature: 5.57e6,
   },
+
+  Acetalmide: {
+    name: 'Acetalmide',
+    formula: 'C2H5NO',
+    CAS: '60-35-5',
+    C1: 125.81,
+    C2: -12.376,
+    C3: -14.589,
+    C4: 5.0824e-6,
+    C5: 2,
+    minimumTemperature: 35.3,
+    maximumTemperature: 3.36e2,
+    pressureAtMinimumTemperature: 761,
+    pressureAtMaximumTemperature: 6.569e6,
+  },
+
+  AceticAcid: {
+    name: 'Acetic acid',
+    formula: 'C2H4O2',
+    CAS: '64-19-7',
+    C1: 5.27,
+    C2: -604.5,
+    C3: -4.298,
+    C4: 8.89e-18,
+    C5: 6,
+    minimumTemperature: 289.81,
+    maximumTemperature: 1.28,
+    pressureAtMinimumTemperature: 591.95,
+    pressureAtMaximumTemperature: 5.739e6,
+  },
 } as const;
 
 export function calculateDIPPRVaporPressure(

5cfcabeb523fa987e2737559c5fdd86f73fdfbe5
qurban <ahmad76qurban@gmail.com>
2021-08-09 14:26:23 +0530

    fix: removed the multiple return statement and placed elements to describe block

--- test/elements.test.ts
+++ test/elements.test.ts
@@ -1,15 +1,14 @@
 import { ElementDictionary } from "../src/data/elements";
 import { Phases, Phase } from "../src/interfaces/element-props";
 
-const elements: string[] = Object.keys(ElementDictionary);
-
 function checkUrl(url: string): boolean {
+  let isUrl: boolean = true;
   try {
     new URL(url);
   } catch (e) {
-    return false;
+    isUrl = false;
   }
-  return true;
+  return isUrl;
 }
 
 function checkPhase(phase: Phase): boolean {
@@ -17,6 +16,7 @@ function checkPhase(phase: Phase): boolean {
 }
 
 describe("Periodic Table Elements", () => {
+  const elements: string[] = Object.keys(ElementDictionary);
   it("should return true for correct URL of source", () => {
     for (let el of elements) {
       expect(checkUrl(ElementDictionary[el].source)).toBe(true);

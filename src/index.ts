3485baf0d7ebc3243f97031055e0ab49a074e216
maneetgoyal <maneetgoyal14@gmail.com>
2021-08-16 09:50:08 +0530

    style: Rename file

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from "./data/inchi-dictionary";
 export * from "./data/food-compounds";
 export * from "./data/density-constants";
 export * from "./data/thermal-conductivity-constants";
+export * from "./data/vapor-thermal-conductivity-constants";
 export * from "./data/critical-constants-acentric-factor";

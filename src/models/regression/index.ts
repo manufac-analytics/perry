f1bf31cbaef37c881e847b480896cc9c2c457d85
maneetgoyal <maneetgoyal14@gmail.com>
2022-01-08 21:01:20 +0530

    chore: Add kombi

--- src/models/regression/index.ts
+++ src/models/regression/index.ts
@@ -1,5 +1,6 @@
 import { RandomForestRegression } from "ml-random-forest";
 import MultivariateLinearRegression from "ml-regression-multivariate-linear";
 import { RobustPolynomialRegression } from "ml-regression-robust-polynomial";
+import { kombi } from "kombi";
 
-console.log(RandomForestRegression, MultivariateLinearRegression, RobustPolynomialRegression);
+console.log(RandomForestRegression, MultivariateLinearRegression, RobustPolynomialRegression, kombi);

c937ff0ec50498a0e014d3fd60ffe792e6725a72
maneetgoyal <maneetgoyal14@gmail.com>
2022-01-08 18:49:23 +0530

    chore: Add needed regression deps

---
+++ src/models/regression/index.ts
@@ -0,0 +1,6 @@
+import { RandomForestRegression } from "ml-random-forest";
+import ExponentialRegression from "ml-regression-exponential";
+import MultivariateLinearRegression from "ml-regression-multivariate-linear";
+import { RobustPolynomialRegression } from "ml-regression-robust-polynomial";
+
+console.log(RandomForestRegression, ExponentialRegression, MultivariateLinearRegression, RobustPolynomialRegression);

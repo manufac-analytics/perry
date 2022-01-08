101fecdcc8eb37a093b990886e222d499e940d2e
maneetgoyal <maneetgoyal14@gmail.com>
2022-01-08 18:53:04 +0530

    chore: Remove `ml-regression-exponential`

--- src/models/regression/index.ts
+++ src/models/regression/index.ts
@@ -1,6 +1,5 @@
 import { RandomForestRegression } from "ml-random-forest";
-import ExponentialRegression from "ml-regression-exponential";
 import MultivariateLinearRegression from "ml-regression-multivariate-linear";
 import { RobustPolynomialRegression } from "ml-regression-robust-polynomial";
 
-console.log(RandomForestRegression, ExponentialRegression, MultivariateLinearRegression, RobustPolynomialRegression);
+console.log(RandomForestRegression, MultivariateLinearRegression, RobustPolynomialRegression);

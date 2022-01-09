ecdb8e83f3625b392530d622859df7b0c0273514
maneetgoyal <maneetgoyal14@gmail.com>
2022-01-09 12:35:40 +0530

    .csv ==> .json

--- src/models/regression/index.ts
+++ src/models/regression/index.ts
@@ -1,10 +1,9 @@
-import fs from "fs";
 import { RandomForestRegression } from "ml-random-forest";
 import MultivariateLinearRegression from "ml-regression-multivariate-linear";
 import { RobustPolynomialRegression } from "ml-regression-robust-polynomial";
 import { kombi } from "kombi";
+import data from "./WineQualityDataSetRed.json";
 
-export const dataSet = fs.readFileSync("WineQualityDataSetRed.csv")
-console.log(dataSet);
+console.log(data);
 console.log(RandomForestRegression, MultivariateLinearRegression, RobustPolynomialRegression, kombi);
 

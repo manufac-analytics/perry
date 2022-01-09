6b1bf77a735a248232d0e55f34d410f85f9c2b39
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-01-09 10:29:23 +0530

    feat: add csv file and regression modules

--- src/models/regression/index.ts
+++ src/models/regression/index.ts
@@ -1,5 +1,8 @@
+import fs from "fs";
 import { RandomForestRegression } from "ml-random-forest";
 import MultivariateLinearRegression from "ml-regression-multivariate-linear";
 import { RobustPolynomialRegression } from "ml-regression-robust-polynomial";
 
+export const dataSet = fs.readFileSync("WineQualityDataSetRed.csv")
+console.log(dataSet);
 console.log(RandomForestRegression, MultivariateLinearRegression, RobustPolynomialRegression);

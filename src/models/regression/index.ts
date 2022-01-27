4b7e1c117c5d659274b39cf9bd7ede57a8c136ea
maneetgoyal <maneetgoyal14@gmail.com>
2022-01-27 15:15:20 +0530

    refactor: Add hyperparamater evaulation routine

--- src/models/regression/index.ts
+++ src/models/regression/index.ts
@@ -16,14 +16,14 @@ const calculateSuccessPercentage = (predicted: number[], observed: number[]) => 
   let successCount = 0;
   for (let i = 0; i < predicted.length; i++) {
     const squareError = predicted[i] - observed[i];
-    if ( squareError === 0){
+    if (squareError === 0) {
       successCount++;
     }
   }
   return (successCount / predicted.length) * 100;
 }
 
-const inputs = data.map((el) => Object.values(el).slice(0,-1));
+const inputs = data.map((el) => Object.values(el).slice(0, -1));
 const outputs = data.map((el) => el.quality);
 const TRAINING_LENGTH = data.length * 95 / 100; // 95 % of the whole data
 const trainX = inputs.slice(0, TRAINING_LENGTH);
@@ -34,44 +34,42 @@ const predictY = outputs.slice(TRAINING_LENGTH);
 // MultivariateLinearRegression
 const arrayTrainY = trainY.map((el) => [el]); // needed because MultivariateLinearRegression contains multiple dependent and independent variables
 let startTime = Date.now();
-const MVLRModel = new MultivariateLinearRegression(trainX, arrayTrainY, { intercept: false ,statistics: false });
+const MVLRModel = new MultivariateLinearRegression(trainX, arrayTrainY, { intercept: false, statistics: false });
 const MVLRPredictions = MVLRModel.predict(predictX).map(([el]) => Math.round(el));
 let endTime = Date.now();
 console.log("MultivariateLinearRegression");
 console.log(`Execution Time:  ${(endTime - startTime).toString()} ms`);
 console.log("Root Mean Square Error:" + rootMeanSquareError(MVLRPredictions, predictY));
 console.log("Success Percentage:" + calculateSuccessPercentage(MVLRPredictions, predictY));
-
+console.log("-----");
 
 // RandomForestRegressions
-const options = {
-  maxFeatures: 11,
-  replacement: false,
-  nEstimators: 500,
-  treeOptions: {
-    maxDepth: 10,
-  }
-};
-const RFRModel = new RandomForestRegression(options);
-startTime = Date.now();
-RFRModel.train(trainX, trainY);
-const RFRPredictions = RFRModel.predict(predictX).map((el) => Math.round(el));
-endTime = Date.now();
-console.log("RandomForestRegressions")
-console.log(`Execution Time:  ${(endTime - startTime).toString()} ms`);
-console.log("Root Mean Square Error:" + rootMeanSquareError(RFRPredictions, predictY));
-console.log("Success Percentage:" + calculateSuccessPercentage(RFRPredictions, predictY));
 
 // hyperperameter combinations for random forest regression
 const availableOptions = {
-  maxFeatures: [7, 8, 9, 10, 11],
+  maxFeatures: [9, 10, 11],
   replacement: [true, false],
-  nEstimators: [300, 400, 500],
+  nEstimators: [500, 600, 700],
   seed: [42],
   useSampleBagging: [true, false],
   noOOB: [true, false],
   isClassifier: [false],
   selectionMethod: ["mean", "median"],
+  treeOptions: [{ maxDepth: 100 }, { maxDepth: 200 }]
 }
 const hyperPerameterCombinations = kombi(availableOptions);
-console.log(hyperPerameterCombinations);
+for (let i = 0; i < hyperPerameterCombinations.length; i += 1) {
+  console.log(`Iteration ${i} of ${hyperPerameterCombinations.length}`);
+  const currentOptions = hyperPerameterCombinations[i];
+  console.log(JSON.stringify(currentOptions, undefined, 2));
+  // @ts-expect-error diiiu
+  const currentRFRModel = new RandomForestRegression(currentOptions);
+  const startTime = Date.now();
+  currentRFRModel.train(trainX, trainY);
+  const endTime = Date.now();
+  const cuurentRFRPredictions = currentRFRModel.predict(predictX).map((el) => Math.round(el));
+  console.log("Root Mean Square Error:" + rootMeanSquareError(cuurentRFRPredictions, predictY));
+  console.log("Success Percentage:" + calculateSuccessPercentage(cuurentRFRPredictions, predictY));
+  console.log(`Training Time:  ${((endTime - startTime) / (60 * 1000)).toString()} minutes`);
+  console.log("-----");
+}
